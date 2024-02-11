0a716883c7e82fd13185c518260c994bdd36174d
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2024-02-11 18:56:09 +0200

    nextTry

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.enableCors({
     origin: process.env.CLIENT_URL,
